c4bb8fcbe6bde8eb80d0f303f330e8bca877c0e5
Muhammad Ghalib <mg.gabe108@gmail.com>
2019-06-13 14:45:26 +1000

    Slingshot prople

---
+++ Assets/Scripts/BuildingController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BuildingState
+{
+    DEFAULT,
+    PICKED_UP,
+    DESTROYED,
+
+    COUNT
+}
+
+public class BuildingController : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}

592e14e4258331a346b8a43d11e6a4a159a4b2cc
Lachlan <lmesman2000@gmail.com>
2019-06-13 14:49:04 +1000

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/Highscore.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Highscore : MonoBehaviour
+{
+
+    public Text currentScoreText;
+    public Text highScoreText;
+
+    private int highScore = 0;
+    private int currentScore = 0;
+
+    void Start()
+    {
+        highScore = PlayerPrefs.GetInt("Highscore", 0);
+        highScoreText.text = highScore.ToString();
+    }
+
+    void Update()
+    {
+        currentScoreText.text = currentScore.ToString();
+
+        if(currentScore > highScore)
+        {
+            highScore = currentScore;
+            highScoreText.text = highScore.ToString();
+        }
+    }
+
+    //Called anytime this object is disabled (changing scenes and quitting app)
+    private void OnDisable()
+    {
+        PlayerPrefs.SetInt("Highscore", highScore);
+        PlayerPrefs.Save();
+    }
+}

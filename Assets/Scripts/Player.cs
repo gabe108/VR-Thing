7ab8ffab8bfdb6b05709ad18f3c7114e569854d0
Muhammad Ghalib <mg.gabe108@gmail.com>
2019-06-21 13:22:06 +1000

    Yeet to start

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,27 +14,20 @@ public class Player : MonoBehaviour
 
     void Update()
     {        
-        if (Input.GetKey(KeyCode.P))
-        {
-            //Destroy(vrgGrabber.m_grabbedObject.gameObject);
-            StartCoroutine(wait());
-        }
+        //if (Input.GetKey(KeyCode.P))
+        //{
+        //    //Destroy(vrgGrabber.m_grabbedObject.gameObject);
+        //    StartCoroutine(wait());
+        //}
 
-        if (vrgGrabber.m_grabbedObject == null)
-            return;
-        else if (vrgGrabber.m_grabbedObject.CompareTag("Start"))
-        {
-            Destroy(vrgGrabber.m_grabbedObject.gameObject);
-            StartCoroutine(wait());           
-        }
+        //if (vrgGrabber.m_grabbedObject == null)
+        //    return;
+        //else if (vrgGrabber.m_grabbedObject.CompareTag("Start"))
+        //{
+        //    Destroy(vrgGrabber.m_grabbedObject.gameObject);
+        //    StartCoroutine(wait());           
+        //}
     }
 
-    private IEnumerator wait()
-    {
-        StartCoroutine(GameObject.FindObjectOfType<OVRScreenFade>().Fade(0, 1));
-        yield return new WaitForSeconds(3f);
-        PlayerPrefs.Save();
-        SceneManager.LoadScene(1);
-        yield return null;
-    }
+   
 }
---
+++ Assets/Scripts/YeetToStart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class YeetToStart : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.transform, 1f);
+        StartCoroutine(wait());
+    }
+    private IEnumerator wait()
+    {
+        StartCoroutine(GameObject.FindObjectOfType<OVRScreenFade>().Fade(0, 1));
+        yield return new WaitForSeconds(3f);
+        PlayerPrefs.Save();
+        SceneManager.LoadScene(1);
+        yield return null;
+    }
+}
